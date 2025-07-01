6406b80fa4a91dfdd3105d02df9321a599a74d48
Guilherme Marques <guilherme_mds@yahoo.com>
2025-07-01 16:30:12 -0300

    Adicionar link do GitHub no footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,17 @@ import styles from './Footer.module.css';
 import Dogs from '../Assets/dogs-footer.svg?react';
 
 const Footer = () => {
-  return <section className={styles.footer}>
-    <Dogs />
-    <p>Powered by Guilherme Marques</p>
-  </section>;
+  return (
+    <section className={styles.footer}>
+      <Dogs />
+      <p>
+        Powered by{' '}
+        <a href="https://github.com/GuilhermeMarques11" target="_blank">
+          Guilherme Marques
+        </a>
+      </p>
+    </section>
+  );
 };
 
 export default Footer;
